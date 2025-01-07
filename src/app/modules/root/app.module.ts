dc60ce0080b52c4f50a3e075d104db7643081cdf
zizou <ziedounaiess@gmail.com>
2025-01-07 18:40:14 +0100

    Resolved merge conflicts

---
+++ src/app/modules/root/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './views/app/app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
